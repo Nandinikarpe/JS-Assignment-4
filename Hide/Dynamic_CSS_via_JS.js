c3299f27625553d5f354aa5034d1095b52f0d157
Nandini Karpe <nandinikarpe8@gmail.com>
2025-07-05 11:34:23 +0530

    Create Dynamic_CSS_via_JS.js

---
+++ Hide/Dynamic_CSS_via_JS.js
@@ -0,0 +1,9 @@
+document.getElementById("styleButton").addEventListener("click", function () {
+    const box = document.getElementById("colorBox");
+    box.style.backgroundColor = "lightblue";
+    box.style.border = "2px solid blue";
+    box.style.borderRadius = "10px";
+    box.style.padding = "20px";
+});
+
+
